d88d3ca92f708fcb9e167de2127c089dc914d17f
MarcusfPetersen <folkmar.petersen@gmail.com>
2026-08-11 15:54:03 +0200

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://yuhetjzkxtoistbtzchy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_VeebKToyxulsAlB7k4_bNg_S8fhn-aR";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
